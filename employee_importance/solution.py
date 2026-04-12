5d68d0690341a469f71a626f8f257e6896170273
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-12 20:50:20 +0300

    Uploaded an image and fixed pylint

--- employee_importance/solution.py
+++ employee_importance/solution.py
@@ -9,24 +9,26 @@ class Employee:
 from typing import List
 
 class Employee:
+    """Class Employee"""
     def __init__(self, id: int, importance: int, subordinates: List[int]):
         self.id = id
         self.importance = importance
         self.subordinates = subordinates
 
 class Solution:
+    """Class that counts importance of emplyees"""
     def find_employee(self, employees: List['Employee'], id: int) -> Employee:
+        """Finds employee by his id"""
         for employee in employees:
             if employee.id == id:
                 return employee
 
     def getImportance(self, employees: List['Employee'], id: int) -> int:
+        """Function that finds importance"""
         root_employee = self.find_employee(employees, id)
         number = root_employee.importance
 
         for employee in root_employee.subordinates:
-            # person = self.find_employee(employees, employee)
-            # number += person.importance
 
             number += self.getImportance(employees, employee)
 
