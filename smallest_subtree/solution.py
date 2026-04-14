cdeca77d1365cb348f55b116c6eced96b3b6abc5
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-14 22:10:46 +0300

    Initializing a function

--- smallest_subtree/solution.py
+++ smallest_subtree/solution.py
@@ -9,6 +9,20 @@ class TreeNode:
         self.right = right
 
 class Solution:
+    def find_the_deepest(self, root: Optional[TreeNode], depth=0):
+        if root is None:
+            return 0, root
+        else:
+            left_depth, left_node = self.find_the_deepest(root.left, depth+1)
+            right_depth, right_node = self.find_the_deepest(root.right, depth+1)
+
+            if left_depth > right_depth:
+                return left_depth+1, left_node
+            elif left_depth < right_depth:
+                return right_depth+1, right_node
+            else:
+                return left_depth+1, root
+
 
     def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        ...
+        return self.find_the_deepest(root)[1]
